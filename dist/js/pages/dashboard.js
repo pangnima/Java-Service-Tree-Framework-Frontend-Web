fc9f89478f0b7e4d00ce04f7cfe1f17e802624fd
LeeDongMin <admin@313.co.kr>
2018-04-02 09:34:38 +0900

    compare spec  layer fixing

--- dist/js/pages/dashboard.js
+++ dist/js/pages/dashboard.js
@@ -124,6 +124,7 @@ $(function () {
 
   /* Morris.js Charts */
   // Sales chart
+  /*
   var area = new Morris.Area({
     element: 'revenue-chart',
     resize: true,
@@ -174,8 +175,9 @@ $(function () {
     gridTextFamily: "Open Sans",
     gridTextSize: 10
   });
-
+  */
   //Donut Chart
+  /*
   var donut = new Morris.Donut({
     element: 'sales-chart',
     resize: true,
@@ -187,7 +189,7 @@ $(function () {
     ],
     hideHover: 'auto'
   });
-
+  */
   //Fix for charts under tabs
   $('.box ul.nav a').on('shown.bs.tab', function () {
     area.redraw();
